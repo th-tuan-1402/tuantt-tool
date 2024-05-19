56b7215801062184e2a35384367dd2819ae0d77b
tt-tuan <tuan.140296@gmail.com>
2024-05-19 12:34:41 +0700

    Adjust demo page

--- pages/media/index.vue
+++ pages/media/index.vue
@@ -1,51 +1,89 @@
 <template>
   <div class="w-1/2 mx-auto">
     <v-card>
-      <v-form>
-        <v-text-field label="Video URL" v-model="videoUrl"></v-text-field>
-        <v-btn @click="$refs.player.playFromRawUrl(videoUrl)">Play</v-btn>
-      </v-form>
-    </v-card>
-    <v-card>
-      <v-form>
-        <v-text-field label="Nhãn" v-model="timeLabel"></v-text-field>
-        <v-btn
-          :disabled="!timeLabel"
-          @click="onCrateTimeLabel"
-          color="green"
-          variant="text"
-          >Mark</v-btn
-        >
-      </v-form>
-    </v-card>
-    <v-card class="d-flex justify-center">
-      <media-player ref="player" :options="videoOptions"></media-player>
-    </v-card>
-    <v-card class="d-flex">
-      <div class="basis-1/2">
-        <v-list density="compact">
-          <v-list-item v-for="(item, i) in mediaSources" :key="i" color="primary">
-            <!-- <template v-slot:prepend>
-            <v-icon :icon="item.icon"></v-icon>
-          </template> -->
-
-            <v-list-item-title
-              v-text="item.src"
+      <v-card-text class="d-flex justify-center">
+        <media-player ref="player" :options="videoOptions"></media-player>
+      </v-card-text>
+      <v-card-text v-if="mediaSources.length > 0" class="d-flex border-t">
+        <div class="basis-1/2 grow">
+          <v-list density="compact">
+            Danh sách phát
+            <v-list-item
+              v-for="(item, i) in mediaSources"
+              :key="i"
+              color="primary"
+              nav
+              :value="item"
               @click="onChangeMediaIndex(i, item)"
-            ></v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </div>
-      <div v-if="mediaSources[currentMediaIndex].labels.length > 0">
-        <v-list density="compact">
-          <v-list-item v-for="(label, index) in currentVideoLabelList" :key="index">
-            <v-list-item-title @click="onPlayMediaFromTimestamp(item, label.timestamp)">
-              <span class="text-primary">{{ timeLabels(label.timestamp) }} </span>
-              {{ label.label }}
-            </v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </div>
+            >
+              <v-list-item-title v-text="item.title"></v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </div>
+        <div
+          v-if="mediaSources[currentMediaIndex]?.labels.length > 0"
+          class="ml-2 basis-1/2"
+        >
+          <v-list density="compact">
+            Nhãn
+            <v-list-item
+              v-for="(label, index) in currentVideoLabelList"
+              :key="index"
+              color="primary"
+              nav
+              :value="label"
+              @click="onPlayMediaFromTimestamp(item, label.timestamp)"
+            >
+              <v-list-item-title>
+                <span class="text-primary">{{
+                  timeLabels(label.timestamp)
+                }}</span>
+                {{ label.label }}
+              </v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </div>
+      </v-card-text>
+    </v-card>
+    <v-card class="mt-4">
+      <v-card-text>
+        <!-- video source panel -->
+        <div class="d-flex">
+          <v-text-field
+            label="Video URL"
+            v-model="videoUrl"
+            class="basis-1/2"
+            density="compact"
+          ></v-text-field>
+          <v-text-field
+            label="Title"
+            v-model="videoTitle"
+            class="basis-1/2 ml-2"
+            density="compact"
+          ></v-text-field>
+          <v-btn
+            @click="onAddMediaSource"
+            :disabled="!videoUrl || !videoTitle"
+            variant="text"
+            color="green"
+            >➕</v-btn
+          >
+        </div>
+        <div class="d-flex">
+          <v-text-field
+            label="Nhãn"
+            v-model="timeLabel"
+            density="compact"
+          ></v-text-field>
+          <v-btn
+            :disabled="!timeLabel"
+            @click="onCrateTimeLabel"
+            color="green"
+            variant="text"
+            >➕</v-btn
+          >
+        </div>
+      </v-card-text>
     </v-card>
   </div>
 </template>
@@ -60,22 +98,11 @@ export default {
     return {
       currentMediaIndex: 0,
       videoUrl: null,
-      mediaSources: [
-        {
-          src: "https://vjs.zencdn.net/v/oceans.mp4",
-          type: "video/mp4",
-          labels: [],
-        },
-        {
-          src: "https://d2zihajmogu5jn.cloudfront.net/elephantsdream/ed_hd.mp4",
-          type: "video/mp4",
-          labels: [],
-        },
-      ],
+      videoTitle: null,
+      mediaSources: [],
       videoOptions: {
         autoplay: true,
         controls: true,
-        sources: [],
       },
       timeLabel: null,
     };
@@ -91,6 +118,16 @@ export default {
     },
   },
   methods: {
+    onAddMediaSource() {
+      this.mediaSources.push({
+        title: this.videoTitle,
+        src: this.videoUrl,
+        type: this.guessVideoType(this.videoUrl),
+        labels: [],
+      });
+      this.videoUrl = null;
+      this.videoTitle = null;
+    },
     onChangeMediaIndex(index, newSource) {
       this.currentMediaIndex = index;
       this.$refs.player.pause();
@@ -100,7 +137,10 @@ export default {
     onCrateTimeLabel() {
       let label = this.timeLabel;
       let timestamp: number = this.$refs.player.getCurrentTime();
-      this.mediaSources[this.currentMediaIndex].labels.push({ label, timestamp });
+      this.mediaSources[this.currentMediaIndex].labels.push({
+        label,
+        timestamp,
+      });
       this.timeLabel = null;
     },
     onPlayMediaFromTimestamp(source, timestamp) {
@@ -108,6 +148,18 @@ export default {
       this.$refs.player.setMediaSources(source);
       this.$refs.player.setCurrentTime(timestamp);
       this.$refs.player.play();
+    },
+    guessVideoType(url: string) {
+      let videoType = null;
+      if (url.includes("mp4")) {
+        videoType = "video/mp4";
+      } else if (url.includes("webm")) {
+        videoType = "video/webm";
+      } else if (url.includes("ogg")) {
+        videoType = "video/ogg";
+      }
+
+      return videoType;
     },
   },
 };
