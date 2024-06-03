d4114864a1aa8adc2a1098136091a4fb663807c4
[Lampart] Tran Thanh Tuan <tran_thanh_tuan@lampart-vn.com>
2024-06-03 15:22:46 +0700

    editor ui

--- pages/tools/tts.vue
+++ pages/tools/tts.vue
@@ -6,24 +6,68 @@
       indeterminate
       v-show="isProcessing"
     ></v-progress-circular>
-    <v-card-text>
-      <v-textarea label="Text to convert" v-model="txtInp"></v-textarea>
-      <v-btn color="primary" @click="onConvert">Convert</v-btn>
+    <v-expansion-panels>
+      <v-expansion-panel title="⚙️">
+        <v-expansion-panel-text>
+          <v-text-field
+            label="File name"
+            v-model="fileName"
+            @change="onChangeTextInput"
+          ></v-text-field>
+        </v-expansion-panel-text>
+      </v-expansion-panel>
+    </v-expansion-panels>
+    <v-card-text v-if="downloadUrl" class="d-flex justify-center">
+      <audio controls>
+        <source :src="downloadUrl" type="audio/mpeg" />
+      </audio>
     </v-card-text>
-    <v-card-text v-if="downloadUrl">
-      <v-text-field
-        label="File name"
-        v-model="fileName"
+    <v-card-text>
+      <v-textarea
+        label="Text to convert"
+        v-model="txtInp"
         @change="onChangeTextInput"
-      ></v-text-field>
-      <v-btn
-        color="green"
-        class="ml"
-        :href="downloadUrl"
-        :download="downloadFileName"
-        target="_blank"
-        >Download</v-btn
-      >
+      ></v-textarea>
+      <div class="d-flex gap-x-1">
+        <v-btn color="primary" @click="onConvert"
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+            />
+          </svg>
+        </v-btn>
+        <v-btn
+          color="green"
+          class="ml"
+          :href="downloadUrl"
+          :download="downloadFileName"
+          target="_blank"
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+            variant="text"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M7.5 7.5h-.75A2.25 2.25 0 0 0 4.5 9.75v7.5a2.25 2.25 0 0 0 2.25 2.25h7.5a2.25 2.25 0 0 0 2.25-2.25v-7.5a2.25 2.25 0 0 0-2.25-2.25h-.75m-6 3.75 3 3m0 0 3-3m-3 3V1.5m6 9h.75a2.25 2.25 0 0 1 2.25 2.25v7.5a2.25 2.25 0 0 1-2.25 2.25h-7.5a2.25 2.25 0 0 1-2.25-2.25v-.75"
+            />
+          </svg>
+        </v-btn>
+      </div>
     </v-card-text>
   </v-card>
 </template>
