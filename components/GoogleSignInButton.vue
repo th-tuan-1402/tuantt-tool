debbe92fe7d09064b0c53dbe22378092ae2cdd85
thanh_tuan <tuan.140296@gmail.com>
2024-11-21 16:12:11 +0700

    + add   - unauthorized layout   - google signin button

---
+++ components/GoogleSignInButton.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import {
+  GoogleSignInButton,
+  type CredentialResponse,
+  decodeCredential
+} from "vue3-google-signin";
+
+// handle success event
+const handleLoginSuccess = (response: CredentialResponse) => {
+  const { credential } = response;
+  const decodedCredential = decodeCredential(credential);
+  console.log("Access Token", credential);
+  console.log("Credential", decodedCredential);
+};
+
+// handle an error event
+const handleLoginError = () => {
+  console.error("Login failed");
+};
+</script>
+
+<template>
+  <GoogleSignInButton
+    @success="handleLoginSuccess"
+    @error="handleLoginError"
+  ></GoogleSignInButton>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <GoogleSignInButton v-if="!isLoggedIn" />
+  <slot v-else />
+</template>
+
+<script lang="ts">
+export default {
+  computed: {
+    isLoggedIn() {
+      return false;
+    },
+  },
+};
+</script>
