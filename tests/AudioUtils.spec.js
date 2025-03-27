147d9eaa1a2381643f6db8f35b39d2fc64aa6af1
thanh_tuan <tran_thanh_tuan@lampart-vn.com>
2025-03-27 16:42:31 +0700

    fix test

--- tests/AudioUtils.spec.js
+++ tests/AudioUtils.spec.js
@@ -1,15 +1,17 @@
 import { describe, it, expect, vi } from 'vitest';
-import audioUtils from '~/scripts/utils/AudioUtils';
+import AudioUtils from '~/scripts/utils/AudioUtils';
 
 vi.mock('crunker', () => ({
   default: vi.fn()}));
 
 describe('AudioUtils constructor', () => {
   it('should initialize crunker correctly', () => {
+    const audioUtils = new AudioUtils();
     expect(audioUtils.crunker).toBeDefined();
   });
 
   it('should be an instance of Crunker', () => {
+    const audioUtils = new AudioUtils();
     expect(audioUtils.crunker).toBeDefined();
   });
 });
